1b7a088966ef40912ed113fe70eb2422ef6cf4b7
Ebony <ebony.williams@gmail.com>
2024-12-04 14:27:01 -0500

    Update next.config.ts for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  assetPrefix: isProduction ? '/product-list-cart/' : undefined, // Adjust for GitHub Pages
+  basePath: isProduction ? '/product-list-cart' : '', // Adjust for GitHub Pages
+  trailingSlash: true,
 };
 
 export default nextConfig;
